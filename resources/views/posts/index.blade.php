b460153217d5adc6295d99da5515afb3ec45ebed
Miyahara3838 <hiroakimiyahra1102@gmail.com>
2023-10-14 15:56:41 +0900

    ブログ投稿作成処理用controller実装

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,12 +10,12 @@
     </head>
     <body class="antialiased">
         <h1>Blog Name</h1>
+        <a href='/posts/create'>create</a>
         <div class="posts">
             @foreach($posts as $post)
                 <div class="post">
-                    <h2 class="tittle">
-                        {{ $post->tittle }}
-                    </h2>
+                    <div class='post'>
+                        <a href="/posts/{{$post->id}}"><h2 class='title'>{{$post->title}}</h2></a>
                     <p class="body">{{ $post->body }}</p>
                 </div>
             @endforeach
